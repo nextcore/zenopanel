cc0785a0e09df6c239e8b312c2bab4bc39c297ba
nextcore <arif.scient@gmail.com>
2026-06-09 15:22:03 +0700

    chore: remove documentation directory and project files

--- DOCS/docs/.vitepress/theme/index.js
+++
@@ -1,9 +0,0 @@
-import DefaultTheme from 'vitepress/theme'
-import './custom.css'
-
-export default {
-    extends: DefaultTheme,
-    enhanceApp({ app, router, siteData }) {
-        // custom app enhancements
-    }
-}
